2cb1c136db7befc2b4149e9175817724ff26cd8c
Sam Richard <snugug@gmail.com>
2014-03-29 08:02:30 -0400

    Build aspects for components

--- component/index.js
+++ component/index.js
@@ -126,6 +126,23 @@ var ComponentGenerator = yeoman.generators.NamedBase.extend({
         'variables': 'Variable Defaults'
       };
 
+      // Aspect Styling
+      var aspects = '\n';
+      for (var i in this.aspects) {
+        aspects += '\n';
+        aspects += '//////////////////////////////\n';
+        aspects += '// @{' + this.slug + '--' + this.aspects[i].toLowerCase() + '}\n';
+        aspects += '// ' + _s.titleize(this.aspects[i]) + ' styling for ' + this.name + ' Component\n';
+        aspects += '.' + this.slug + '--' + _s.slugify(this.aspects[i]).toUpperCase() + ' {\n';
+        aspects += '  \n';
+        aspects += '}\n';
+        aspects += '// {' + this.slug + '--' + this.aspects[i].toLowerCase() + '}@\n';
+        aspects += '//////////////////////////////\n';
+        aspects += '\n';
+      }
+
+      this.aspects = aspects;
+
       this.template('main.scss', dir + '/_' + this.slug + '.scss');
       for (var i in types) {
         this.type = types[i];
