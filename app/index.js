59581a87b687819440d14c10f70cb211bb43f790
Sam Richard <snugug@gmail.com>
2014-03-29 08:44:22 -0400

    Have North greet everyone!
    Plus a few tweaks to the ASCII art

--- app/index.js
+++ app/index.js
@@ -37,29 +37,29 @@ var NorthGenerator = yeoman.generators.Base.extend({
     var done = this.async();
 
     var welcome =
-    '\n' + chalk.blue('                 /\\\\') +
-    '\n' + chalk.blue('                //@\\\\') +
-    '\n' + chalk.blue('               // @@\\\\') +
-    '\n' + chalk.blue('              //  @@@\\\\') +
-    '\n' + chalk.blue('             //   @@@@\\\\') +
-    '\n' + chalk.blue('            //    @@@@@\\\\       ') + chalk.cyan('  ___   ___    _____    ______    _________   ___  ___   ') +
-    '\n' + chalk.blue('            //    @@@@@\\\\       ') + chalk.cyan(' |NNN\\ |NNN|  /O.-.O\\  |RRRRRR\\  |TTTTTTTTT| |HHH||HHH|  ') +
-    '\n' + chalk.blue('           //     @@@@@@\\\\      ') + chalk.cyan('  |NNN\\ |N|  |O|   |O|  |R|__)R| |T| |T| |T|  |H|__|H|   ') +
-    '\n' + chalk.blue('          //      @@@@@@@\\\\     ') + chalk.cyan('  |N|\\N\\|N|  |O|   |O|  |RRRRR/      |T|      |HHHHHH|   ') +
-    '\n' + chalk.blue('         //       @@@@@@@@\\\\    ') + chalk.cyan('  |N| \\NNN|  |OO`-\'OO|  |R|  \\R\\_    |T|      |H|  |H|   ') +
-    '\n' + chalk.blue('         //       @@@@@@@@\\\\    ') + chalk.cyan(' |NNN| \\NN|   \\OOOOO/  |RRR| |RRR|  |TTT|    |HHH||HHH|  ') +
-    '\n' + chalk.blue('        //        @@@@@@@@@\\\\      ') +
-    '\n' + chalk.blue('       //         @@@@@@@@@@\\\\     ') +
-    '\n' + chalk.blue('      //        // \\\\@@@@@@@@\\\\  ') +
-    '\n' + chalk.blue('     //      //       \\\\@@@@@@\\\\ ') +
-    '\n' + chalk.blue('    //  //                 \\\\@@\\\\') +
-    '\n' + chalk.blue('    ////                     \\\\\\\\');
+    '\n' + chalk.blue('               /\\\\') +
+    '\n' + chalk.blue('              //@\\\\') +
+    '\n' + chalk.blue('             // @@\\\\') +
+    '\n' + chalk.blue('            //  @@@\\\\') +
+    '\n' + chalk.blue('           //   @@@@\\\\') +
+    '\n' + chalk.blue('          //    @@@@@\\\\        ') + chalk.cyan(' ___   ___    _____    ______    _________   ___  ___   ') +
+    '\n' + chalk.blue('          //    @@@@@\\\\        ') + chalk.cyan('|NNN\\ |NNN|  /O.-.O\\  |RRRRRR\\  |TTTTTTTTT| |HHH||HHH|  ') +
+    '\n' + chalk.blue('         //     @@@@@@\\\\       ') + chalk.cyan(' |NNN\\ |N|  |O|   |O|  |R|__)R| |T| |T| |T|  |H|__|H|   ') +
+    '\n' + chalk.blue('        //      @@@@@@@\\\\      ') + chalk.cyan(' |N|\\N\\|N|  |O|   |O|  |RRRRR/      |T|      |HHHHHH|   ') +
+    '\n' + chalk.blue('       //       @@@@@@@@\\\\     ') + chalk.cyan(' |N| \\NNN|  |OO`-\'OO|  |R|  \\R\\_    |T|      |H|  |H|   ') +
+    '\n' + chalk.blue('       //       @@@@@@@@\\\\     ') + chalk.cyan('|NNN| \\NN|   \\OOOOO/  |RRR| |RRR|  |TTT|    |HHH||HHH|  ') +
+    '\n' + chalk.blue('      //        @@@@@@@@@\\\\      ') +
+    '\n' + chalk.blue('     //         @@@@@@@@@@\\\\     ') +
+    '\n' + chalk.blue('    //        // \\\\@@@@@@@@\\\\  ') +
+    '\n' + chalk.blue('   //      //       \\\\@@@@@@\\\\ ') +
+    '\n' + chalk.blue('  //  //                 \\\\@@\\\\') +
+    '\n' + chalk.blue('  ////                     \\\\\\\\');
 
-    // have Yeoman greet the user
+    // have North greet the user
     this.log(welcome);
 
     // replace it with a short and sweet description of your generator
-    this.log(chalk.magenta('\nA generator to help align and guide your project; a companion for North. ') + chalk.cyan('http://pointnorth.io'));
+    this.log(chalk.magenta('\nA generator to help align and guide your project; a companion for North. ') + chalk.cyan('http://pointnorth.io\n'));
 
     var prompts = [{
       type: 'string',
--- layout/index.js
+++ layout/index.js
@@ -36,7 +36,30 @@ var workingDir = function () {
 
 var LayoutGenerator = yeoman.generators.NamedBase.extend({
   init: function () {
-    // console.log('You called the layout subgenerator with the argument ' + this.name + '.');
+    var welcome =
+    '\n' + chalk.blue('               /\\\\') +
+    '\n' + chalk.blue('              //@\\\\') +
+    '\n' + chalk.blue('             // @@\\\\') +
+    '\n' + chalk.blue('            //  @@@\\\\') +
+    '\n' + chalk.blue('           //   @@@@\\\\') +
+    '\n' + chalk.blue('          //    @@@@@\\\\        ') + chalk.cyan(' ___   ___    _____    ______    _________   ___  ___   ') +
+    '\n' + chalk.blue('          //    @@@@@\\\\        ') + chalk.cyan('|NNN\\ |NNN|  /O.-.O\\  |RRRRRR\\  |TTTTTTTTT| |HHH||HHH|  ') +
+    '\n' + chalk.blue('         //     @@@@@@\\\\       ') + chalk.cyan(' |NNN\\ |N|  |O|   |O|  |R|__)R| |T| |T| |T|  |H|__|H|   ') +
+    '\n' + chalk.blue('        //      @@@@@@@\\\\      ') + chalk.cyan(' |N|\\N\\|N|  |O|   |O|  |RRRRR/      |T|      |HHHHHH|   ') +
+    '\n' + chalk.blue('       //       @@@@@@@@\\\\     ') + chalk.cyan(' |N| \\NNN|  |OO`-\'OO|  |R|  \\R\\_    |T|      |H|  |H|   ') +
+    '\n' + chalk.blue('       //       @@@@@@@@\\\\     ') + chalk.cyan('|NNN| \\NN|   \\OOOOO/  |RRR| |RRR|  |TTT|    |HHH||HHH|  ') +
+    '\n' + chalk.blue('      //        @@@@@@@@@\\\\      ') +
+    '\n' + chalk.blue('     //         @@@@@@@@@@\\\\     ') +
+    '\n' + chalk.blue('    //        // \\\\@@@@@@@@\\\\  ') +
+    '\n' + chalk.blue('   //      //       \\\\@@@@@@\\\\ ') +
+    '\n' + chalk.blue('  //  //                 \\\\@@\\\\') +
+    '\n' + chalk.blue('  ////                     \\\\\\\\');
+
+    // have North greet the user
+    this.log(welcome);
+
+    // replace it with a short and sweet description of your generator
+    this.log(chalk.magenta('\nLayout generator for North. ') + chalk.cyan('http://pointnorth.io\n'));
   },
 
   askFor: function () {
