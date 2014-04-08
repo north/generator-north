ba5ef392dc2e53a78b1efde603287e860efcbb46
Sam Richard <snugug@gmail.com>
2014-04-08 17:18:20 -0400

    Grunt Server options
    Sass/Compass/JS Hint server with BrowserStack

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,18 +1,63 @@
 'use strict';
 
 module.exports = function (grunt) {
-  var options = require('./.compass-options').compassOptions();
+  var paths = require('compass-options').paths();
+  <% if (server) { %>var dirs = require('compass-options').dirs();<% } %>
 
   grunt.initConfig({
     jshint: {
       options: {
         jshintrc: '.jshintrc'
       },
-      all: [options.js + '/{,**/}*.js']
+      all: [
+        paths.js + '/{,**/}*.js',
+        '!' + paths.js + '/{,**/}*.min.js'
+      ]
+    }<% if (server) { %>,
+    compass: {
+      dist: {
+        options: {
+          bundleExec: true,
+          config: './config.rb'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: paths.js + '/{,**/}*.js',
+        tasks: ['jshint']
+      },
+      sass: {
+        files: paths.sass + '/{,**/}*.{scss|sass}',
+        tasks: ['compass']
+      }
+    },
+    browserSync: {
+      dev: {
+        bsFiles: {
+          src: [
+            paths.css +  '/**/*.css',
+            paths.js + '/**/*.js',
+            paths.img + '/**/*',
+            paths.fonts + '/**/*',
+            paths.html + '/**/*.html',
+          ]
+        },
+        options: {
+          watchTask: true,
+          server: {
+            baseDir: paths.html
+          }
+        }
+      }
     }
+    <% } %>
   });
 
   require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
 
   grunt.registerTask('lint', ['jshint']);
+
+  <% if (server) { %>grunt.registerTask('server', ['browserSync', 'watch']);
+  grunt.registerTask('serve', ['server']);<% } %>
 }
