dd05f71608c1cd955420a549e257e04a180aa752
Sam Richard <snugug@gmail.com>
2014-04-08 17:27:48 -0400

    Gulp Server Option
    Sass/Compass/JS Hint server with BrowserStack

--- app/templates/Gulpfile.js
+++ app/templates/Gulpfile.js
@@ -1,13 +1,61 @@
+'use strict';
+
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
-var options = require('./.compass-options').compassOptions();
+var paths = require('compass-options').paths();<% if (server) { %>
+var browserSync = require('browser-sync');
+var shell = require('gulp-shell');<% } %>
 
 //////////////////////////////
 // Begin Gulp Tasks
 //////////////////////////////
 gulp.task('lint', function () {
-  gulp.src(options.js + '/**/*.js')
+  return gulp.src([
+      paths.js + '/**/*.js',
+      '!' + paths.js + '/**/*.js'
+    ])
     .pipe(jshint())
     .pipe(jshint.reporter(stylish))
 });
+<% if (server) { %>
+//////////////////////////////
+// Compass Task
+//////////////////////////////
+gulp.task('compass', function () {
+  return gulp.src(paths.sass + '/**/*')
+    .pipe(shell([
+      'bundle exec compass watch --time'
+    ]));
+});
+
+//////////////////////////////
+// Watch
+//////////////////////////////
+gulp.task('watch', function () {
+  gulp.watch(paths.js + '/**/*.js', ['lint']);
+});
+
+//////////////////////////////
+// BrowserSync Task
+//////////////////////////////
+gulp.task('browserSync', function () {
+  browserSync.init([
+    paths.css +  '/**/*.css',
+    paths.js + '/**/*.js',
+    paths.img + '/**/*',
+    paths.fonts + '/**/*',
+    paths.html + '/**/*.html',
+  ], {
+    server: {
+      baseDir: paths.html
+    }
+  });
+});
+
+//////////////////////////////
+// Server Tasks
+//////////////////////////////
+gulp.task('server', ['watch', 'compass', 'browserSync']);
+gulp.task('serve', ['server']);
+<% } %>
