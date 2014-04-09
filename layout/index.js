1083b8d3396e9014d0860ecbd773957de9d88c0f
Sam Richard <snugug@gmail.com>
2014-04-09 09:03:31 -0400

    Removes Shared require (dunno why not coming through) and replaces with duplicated code :( (For #1)

--- layout/index.js
+++ layout/index.js
@@ -5,7 +5,56 @@ var fs = require('fs');
 var inquirer = require("inquirer");
 var chalk = require('chalk');
 var _s = require('underscore.string');
-var shared = require('../shared.js');
+
+var path = require('path');
+var shared = {};
+shared.workingDir = function () {
+  var dir = path.basename(process.cwd()).toLowerCase();
+  switch (dir) {
+    case 'components':
+      return '.'
+    case 'layouts':
+      return '../components'
+    case 'partials':
+      return './components'
+    case 'sass':
+    case 'scss':
+      return './partials/components'
+    default:
+      if (fs.existsSync('sass')) {
+        return './sass/partials/components'
+      }
+      else if (fs.existsSync('scss')) {
+        return './scss/partials/components'
+      }
+      else {
+        return false;
+      }
+      break;
+  }
+}
+
+shared.welcome = function () {
+  var welcome =
+    '\n' + chalk.blue('               /\\\\') +
+    '\n' + chalk.blue('              //@\\\\') +
+    '\n' + chalk.blue('             // @@\\\\') +
+    '\n' + chalk.blue('            //  @@@\\\\') +
+    '\n' + chalk.blue('           //   @@@@\\\\') +
+    '\n' + chalk.blue('          //    @@@@@\\\\        ') + chalk.cyan(' ___   ___    _____    ______    _________   ___  ___   ') +
+    '\n' + chalk.blue('          //    @@@@@\\\\        ') + chalk.cyan('|NNN\\ |NNN|  /O.-.O\\  |RRRRRR\\  |TTTTTTTTT| |HHH||HHH|  ') +
+    '\n' + chalk.blue('         //     @@@@@@\\\\       ') + chalk.cyan(' |NNN\\ |N|  |O|   |O|  |R|__)R| |T| |T| |T|  |H|__|H|   ') +
+    '\n' + chalk.blue('        //      @@@@@@@\\\\      ') + chalk.cyan(' |N|\\N\\|N|  |O|   |O|  |RRRRR/      |T|      |HHHHHH|   ') +
+    '\n' + chalk.blue('       //       @@@@@@@@\\\\     ') + chalk.cyan(' |N| \\NNN|  |OO`-\'OO|  |R|  \\R\\_    |T|      |H|  |H|   ') +
+    '\n' + chalk.blue('       //       @@@@@@@@\\\\     ') + chalk.cyan('|NNN| \\NN|   \\OOOOO/  |RRR| |RRR|  |TTT|    |HHH||HHH|  ') +
+    '\n' + chalk.blue('      //        @@@@@@@@@\\\\      ') +
+    '\n' + chalk.blue('     //         @@@@@@@@@@\\\\     ') +
+    '\n' + chalk.blue('    //        // \\\\@@@@@@@@\\\\  ') +
+    '\n' + chalk.blue('   //      //       \\\\@@@@@@\\\\ ') +
+    '\n' + chalk.blue('  //  //                 \\\\@@\\\\') +
+    '\n' + chalk.blue('  ////                     \\\\\\\\');
+  return welcome;
+}
 
 
 var LayoutGenerator = yeoman.generators.Base.extend({
